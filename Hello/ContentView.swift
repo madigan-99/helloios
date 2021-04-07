0fa73f931dd494331ce152222ced31394ed44fe1
YuYu Madigan <fymadigan@outlook.com>
2021-04-06 20:12:42 -0700

    adding swift files and screenshot

---
+++ Hello/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hello
+//
+//  Created by stlp on 4/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Go Dawgs!!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by stlp on 4/6/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
